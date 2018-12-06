dad9c7be0d3d2f8b157ca0644103f958f5b72b62
CoderCroc <ryan_gg@hotmail.com>
2018-12-06 14:24:28 +0800

    svmUpdate

--- NB/NB.py
+++ NB/NB.py
@@ -18,7 +18,7 @@ from scipy.stats import chi2_contingency
 np.set_printoptions(suppress=True)
 
 
-def sparseTxt(item_list, mode):
+def parseTitles(titleList, mode):
     # p = PorterStemmer()
     # stopList = stopwords.words('english')
 
@@ -38,14 +38,14 @@ def sparseTxt(item_list, mode):
                 "AT", "YOUR", "THEIRS", "BELOW", "OTHER", "NOT", "RE", "HIM", "DURING", "WHICH"]
 
     urlFilter = r"[W]{3}\.[\w]{2,}\.\w+\.*\w*"
-    patFilter = r"\.{2,}"
+    dotFilter = r"\.{2,}"
 
     if mode == 0:
-        result = [re.split("\s+", re.sub(pattern, " ", items.upper()))[:-1] for items in item_list]  # -1:\n存在于末尾
+        result = [re.split("\s+", re.sub(pattern, " ", items.upper()))[:-1] for items in titleList]  # -1:\n存在于末尾
 
     else:  # 垃圾符号集计入分词列表中
         result = []
-        for items in item_list:
+        for items in titleList:
             patWords = []
 
             if mode == 2:
@@ -57,8 +57,8 @@ def sparseTxt(item_list, mode):
                 temp = temp.replace(matches, " ")
                 patWords.append("#WEBSITE")
 
-            while re.search(patFilter, temp):  # ..*n 归一化
-                temp = temp.replace(re.search(patFilter, temp).group(), " ")
+            while re.search(dotFilter, temp):  # ..*n 归一化
+                temp = temp.replace(re.search(dotFilter, temp).group(), " ")
                 patWords.append("...")
 
             if len(temp) == 0:
@@ -86,7 +86,7 @@ def initTest(mode):
     for rows in range(1, row):
         result.append(booksheet.cell_value(rows, 1))  # upload all test items
         label.append(booksheet.cell_value(rows, 2))
-    result = sparseTxt(result, mode)
+    result = parseTitles(result, mode)
     return result, label
 
 
@@ -95,8 +95,8 @@ def initTrain(mode):  # neg,pos in list
     posTrain = open("../DATA/positive_train.txt", encoding='cp936').readlines()
     negLen = len(negTrain)
     posLen = len(posTrain)
-    negTrain = sparseTxt(negTrain, mode)
-    posTrain = sparseTxt(posTrain, mode)
+    negTrain = parseTitles(negTrain, mode)
+    posTrain = parseTitles(posTrain, mode)
     return negTrain, negLen, posTrain, posLen  # 二维分词数组，title数
 
 
@@ -107,7 +107,7 @@ def createDict(listItems):
         temp = []
         for words in items:
             if words not in result.keys():
-                result[words] = [1, 1]  # word: word count; doc_count
+                result[words] = [1, 1]  # word: word count; doc count
             else:
                 result[words][0] += 1  # 出现的词汇数+1
                 if words not in temp:  # 此title已经加过文档数
@@ -119,69 +119,69 @@ def createDict(listItems):
 
 def calChi(posDict, posTitleNumber, negDict, negDictNum, mode):  # 计算pos/neg 的 chisq 值
     arguments = np.zeros((2, 2))
-    chi_dict = {}
-    l_pos = list(posDict.keys())
-    l_neg = list(negDict.keys())
+    chisqDict = {}
+    posWords = list(posDict.keys())
+    negWords = list(negDict.keys())
 
-    for word in list(set(l_neg + l_pos)):
-        chi_dict[word] = 0.0
+    for word in list(set(negWords + posWords)):
+        chisqDict[word] = 0.0
 
         arguments[1][1] = posDict[word][1] if word in posDict.keys() else 0
         arguments[0][1] = posTitleNumber - arguments[1][1]
         arguments[1][0] = negDict[word][1] if word in negDict.keys() else 0
         arguments[0][0] = negDictNum - arguments[1][0]
 
-        chi_dict[word] = chi2_contingency(arguments)[0]  # pos_chisq
+        chisqDict[word] = chi2_contingency(arguments)[0]  # pos_chisq
 
-    sortPos = sorted(chi_dict.items(), key=lambda d: d[1], reverse=True)
+    sortPos = sorted(chisqDict.items(), key=lambda d: d[1], reverse=True)
     dataWriteCsv("chisq_words_mode_" + str(mode) + ".csv", sortPos)
 
     return sortPos  # 返回chisq数组
 
 
-def dataWriteCsv(file_name, datas):
-    csvFile = codecs.open(file_name, 'w+', 'utf-8')  # 追加
+def dataWriteCsv(fileName, datas):
+    csvFile = codecs.open(fileName, 'w+', 'utf-8')  # 追加
     writer = csv.writer(csvFile, delimiter=' ', quotechar=' ', quoting=csv.QUOTE_MINIMAL)
     for data in datas:
         writer.writerow([data[0], data[1]])
     print("保存文件成功")
 
 
-def inChiTopK(word, chisq_tup):
-    for items in chisq_tup:
+def inChiTopK(word, chisqTuple):
+    for items in chisqTuple:
         if word == items[0]:
             return True
     return False
 
 
-def toBayes(p_pos, p_neg, word_size, chi_ori_tup, posWords, negWords, testLab):
+def toBayes(posProb, negProb, wordSize, chisqIndex, posWords, negWords, testLabel):
     resultList = []
-    decision = chi_ori_tup[:word_size]
+    chisqRange = chisqIndex[:wordSize]
     pinLab = 0
     countTrue = 0
     countFalse = 0
     for titles in testTitle:
-        nb_neg = math.log2(p_pos)
-        nb_pos = math.log2(p_neg)
+        negNB = math.log2(posProb)
+        posNB = math.log2(negProb)
         for words in titles:
-            if inChiTopK(words, decision):
+            if inChiTopK(words, chisqRange):
 
                 if words in posWords.keys():
-                    nb_pos += math.log2((posWords[words][0] + 1) / (poswordcount + posTypes))  # 加一平滑
+                    posNB += math.log2((posWords[words][0] + 1) / (posWordCount + posTypes))  # 加一平滑
                 else:
-                    nb_pos += math.log2(1 / (poswordcount + posTypes))
+                    posNB += math.log2(1 / (posWordCount + posTypes))
                 if words in negWords.keys():
-                    nb_neg += math.log2((negWords[words][0] + 1) / (negWordCount + negTypes))
+                    negNB += math.log2((negWords[words][0] + 1) / (negWordCount + negTypes))
                 else:
-                    nb_neg += math.log2(1 / (negWordCount + negTypes))
+                    negNB += math.log2(1 / (negWordCount + negTypes))
 
-        judge = "Y" if nb_pos > nb_neg else "N"
+        judge = "Y" if posNB > negNB else "N"
 
-        if judge == testLab[pinLab]:
+        if judge == testLabel[pinLab]:
             countTrue += 1
         else:
             countFalse += 1
-            resultList.append([str(titles) + ",", str(judge) + ",", str(testLab[pinLab])])
+            resultList.append([str(titles) + ",", str(judge) + ",", str(testLabel[pinLab])])
         pinLab += 1
     print("error ", countFalse)
 
@@ -191,12 +191,12 @@ if __name__ == '__main__':
     negTrain, negTitleNum, posTrain, posTitleNum = initTrain(mode)  # l:标题数目
     testTitle, testLab = initTest(mode + 1)
     negWords, negWordCount = createDict(negTrain)  # count:number of words
-    posWords, poswordcount = createDict(posTrain)  # create dicts of neg & pos
+    posWords, posWordCount = createDict(posTrain)  # create dicts of neg & pos
     negTypes = len(negWords.keys())
     posTypes = len(posWords.keys())
 
-    chi_ori_tup = calChi(posWords, posTitleNum, negWords, negTitleNum, mode)  # tup: (word,[chisq])
+    chisqIndex = calChi(posWords, posTitleNum, negWords, negTitleNum, mode)  # tup: (word,[chisq])
 
     for i in range(4400, 4800, 100):
         print(i)
-        toBayes(0.5, 0.5, i, chi_ori_tup, posWords, negWords, testLab)
+        toBayes(0.5, 0.5, i, chisqIndex, posWords, negWords, testLab)
